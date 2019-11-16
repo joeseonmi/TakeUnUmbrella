130bef06b247a1bf7b05f9ddde38b024cc58c263
joeseonmi <jemma3136@gmail.com>
2019-11-16 21:43:35 +0900

    Apply .gitignore

--- Pods/Target Support Files/Pods-TakeUnUmbrella-TakeUnUmbrellaUITests/Pods-TakeUnUmbrella-TakeUnUmbrellaUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TakeUnUmbrella_TakeUnUmbrellaUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TakeUnUmbrella_TakeUnUmbrellaUITests
-@end
